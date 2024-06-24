e51ded0b19ddd5275f75fa402cbbbe8371c81182
Paul <pasha.makey@gmail.com>
2024-06-25 02:01:11 +0300

    Current Account List Button Layout

--- BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
+++ BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
@@ -8,8 +8,45 @@
 import SwiftUI
 
 struct CurrentAccountView: View {
+    
+    @State private var currentAccount = AccountsProvider.getAccounts().first
+    private let accounts = AccountsProvider.getAccounts()
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            VStack {
+                List {
+                    HStack() {
+                        
+                        Image("cardImage")
+                            .resizable()
+                            .frame(width: 40, height: 25)
+                            .padding(.top, -30)
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text(currentAccount?.name ?? "")
+                                .font(.headline)
+                                .fontWeight(.bold)
+                                .foregroundStyle(.white)
+                            
+                            Text(currentAccount?.number ?? "")
+                                .fontWeight(.semibold)
+                                .font(.subheadline)
+                                .foregroundStyle(.secondaryText)
+                            
+                            Text(currentAccount?.pin ?? "")
+                                .fontWeight(.semibold)
+                                .font(.subheadline)
+                                .foregroundStyle(.secondaryText)
+                        }
+                        .padding([.top, .bottom], 8)
+                    }
+                }
+            }
+            .navigationTitle("Account")
+        }
     }
 }
 
