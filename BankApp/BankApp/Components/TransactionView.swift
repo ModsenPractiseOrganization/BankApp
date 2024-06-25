9deebaf13c4b9ff5d6d4a26b8e699efd6587d6db
Paul <pasha.makey@gmail.com>
2024-06-25 21:39:05 +0300

    Added Files

---
+++ BankApp/BankApp/Components/TransactionView.swift
@@ -0,0 +1,39 @@
+//
+//  TransactionView.swift
+//  BankApp
+//
+//  Created by Paul Makey on 25.06.24.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    let transaction: Transaction
+    
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text(transaction.company)
+                    .foregroundStyle(.white)
+                    .font(.title3)
+                    .fontWeight(.bold)
+                
+                Text(transaction.date.formatted())
+                    .foregroundStyle(.secondaryText)
+                
+                Text(transaction.status.rawValue)
+                    .foregroundStyle(transaction.status.getColor)
+            }
+            
+            Spacer()
+            
+            HStack {
+                Text(String(format: "%.2f", transaction.amount))
+                    .foregroundStyle(.white)
+                
+                Image("chevron.forward.dark")
+            }
+        }
+        
+    }
+}
