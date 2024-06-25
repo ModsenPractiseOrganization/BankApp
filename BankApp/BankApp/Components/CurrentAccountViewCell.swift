7d66605506de773a56011755e1889d99848f809d
Paul <pasha.makey@gmail.com>
2024-06-25 21:45:37 +0300

    Current Account View Cell Extracted to Separate File

---
+++ BankApp/BankApp/Components/CurrentAccountViewCell.swift
@@ -0,0 +1,53 @@
+//
+//  CurrentAccountViewCell.swift
+//  BankApp
+//
+//  Created by Paul Makey on 25.06.24.
+//
+
+import SwiftUI
+
+struct CurrentAccountViewCell: View {
+    let currentAccount: Account
+    
+    var body: some View {
+        HStack {
+            Image("cardImage")
+                .resizable()
+                .frame(width: 40, height: 25)
+                .padding(.top, -35)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(currentAccount.name)
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                
+                Text(currentAccount.number)
+                    .fontWeight(.semibold)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondaryText)
+                
+                Text(currentAccount.hiddenPin)
+                    .fontWeight(.semibold)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondaryText)
+            }
+            .padding([.top, .bottom], 5)
+            
+            Spacer()
+            
+            Image("chevron.forward")
+        }
+        .padding()
+        .frame(width: UIScreen.main.bounds.width - 8)
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.viewBackground)
+        }
+    }
+}
+
+#Preview {
+    CurrentAccountView()
+}
