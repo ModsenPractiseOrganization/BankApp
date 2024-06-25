f945051368285e576b8fb1875ff940d207d5de4b
Paul <pasha.makey@gmail.com>
2024-06-26 01:14:14 +0300

    Made Custom Button Component

---
+++ BankApp/BankApp/Components/CustomButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  CustomButtonView.swift
+//  BankApp
+//
+//  Created by Paul Makey on 26.06.24.
+//
+
+import SwiftUI
+
+struct CustomButtonView: View {
+    let title: String
+    let action: () -> ()
+    
+    var body: some View {
+        VStack {
+            Button {
+                action()
+            } label: {
+                Text(title)
+                    .foregroundStyle(.white)
+                    .font(.title3)
+                    .fontWeight(.black)
+            }
+            .padding()
+            .frame(width: UIScreen.main.bounds.width - 16)
+            .background {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundStyle(.milkBlue)
+            }
+        }
+    }
+}
