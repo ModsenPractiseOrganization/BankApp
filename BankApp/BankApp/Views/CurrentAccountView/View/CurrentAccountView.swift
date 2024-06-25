203ffb710872e2c619bc814fab1eadc683b19c3c
Paul <pasha.makey@gmail.com>
2024-06-25 21:45:57 +0300

    Using CurrentAccountViewCell

--- BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
+++ BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
@@ -29,7 +29,7 @@ struct CurrentAccountView: View {
             Button {
                 
             } label: {
-                CurrentAccountView()
+                CurrentAccountViewCell(currentAccount: currentAccount!)
             }
             
             // RECENT TRANSACTIONS
@@ -71,44 +71,6 @@ struct CurrentAccountView: View {
             }
             
             Spacer()
-        }
-    }
-    
-    @ViewBuilder
-    func CurrentAccountView() -> some View {
-        HStack {
-            Image("cardImage")
-                .resizable()
-                .frame(width: 40, height: 25)
-                .padding(.top, -35)
-            
-            VStack(alignment: .leading, spacing: 5) {
-                Text(currentAccount?.name ?? "")
-                    .font(.headline)
-                    .fontWeight(.bold)
-                    .foregroundStyle(.white)
-                
-                Text(currentAccount?.number ?? "")
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
-                    .foregroundStyle(.secondaryText)
-                
-                Text(currentAccount?.hiddenPin ?? "")
-                    .fontWeight(.semibold)
-                    .font(.subheadline)
-                    .foregroundStyle(.secondaryText)
-            }
-            .padding([.top, .bottom], 5)
-            
-            Spacer()
-            
-            Image("chevron.forward")
-        }
-        .padding()
-        .frame(width: UIScreen.main.bounds.width - 8)
-        .background {
-            RoundedRectangle(cornerRadius: 20)
-                .foregroundStyle(.viewBackground)
         }
     }
 }
