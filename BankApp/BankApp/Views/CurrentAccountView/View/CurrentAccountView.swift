ff1b0fd3fc14e2fb8ea4474db6be3858e98229ff
Paul <pasha.makey@gmail.com>
2024-06-25 21:40:44 +0300

    Layout is Done

--- BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
+++ BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
@@ -10,42 +10,105 @@ import SwiftUI
 struct CurrentAccountView: View {
     
     @State private var currentAccount = AccountsProvider.getAccounts().first
-    private let accounts = AccountsProvider.getAccounts()
-    
+    private let recentTransactions = RecentTransactionsProvider.getRecentTransactions()
     
     var body: some View {
         
-        NavigationStack {
+        VStack {
+            HStack {
+                Text("Account")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading, 6)
+                
+                Spacer()
+            }
+            .padding(.top, 12)
+            
+            // ACCOUNT
+            Button {
+                
+            } label: {
+                CurrentAccountView()
+            }
+            
+            // RECENT TRANSACTIONS
+            HStack {
+                Text("Recent Transactions")
+                    .font(.title)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("VIEW ALL")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                }
+            }
+            .padding([.leading, .trailing], 6)
+            .padding(.top, 20)
+            
+            // LIST
             VStack {
-                List {
-                    HStack() {
+                ForEach(recentTransactions.prefix(4), id: \.self) { transaciton in
+                    Button {
                         
-                        Image("cardImage")
-                            .resizable()
-                            .frame(width: 40, height: 25)
-                            .padding(.top, -30)
-                        
-                        VStack(alignment: .leading, spacing: 5) {
-                            Text(currentAccount?.name ?? "")
-                                .font(.headline)
-                                .fontWeight(.bold)
-                                .foregroundStyle(.white)
-                            
-                            Text(currentAccount?.number ?? "")
-                                .fontWeight(.semibold)
-                                .font(.subheadline)
-                                .foregroundStyle(.secondaryText)
-                            
-                            Text(currentAccount?.pin ?? "")
-                                .fontWeight(.semibold)
-                                .font(.subheadline)
-                                .foregroundStyle(.secondaryText)
+                    } label: {
+                        VStack {
+                            TransactionView(transaction: transaciton)
+                            Divider()
                         }
-                        .padding([.top, .bottom], 8)
                     }
                 }
             }
-            .navigationTitle("Account")
+            .padding()
+            .background {
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundStyle(.viewBackground)
+            }
+            
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    func CurrentAccountView() -> some View {
+        HStack {
+            Image("cardImage")
+                .resizable()
+                .frame(width: 40, height: 25)
+                .padding(.top, -35)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(currentAccount?.name ?? "")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                
+                Text(currentAccount?.number ?? "")
+                    .fontWeight(.semibold)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondaryText)
+                
+                Text(currentAccount?.hiddenPin ?? "")
+                    .fontWeight(.semibold)
+                    .font(.subheadline)
+                    .foregroundStyle(.secondaryText)
+            }
+            .padding([.top, .bottom], 5)
+            
+            Spacer()
+            
+            Image("chevron.forward")
+        }
+        .padding()
+        .frame(width: UIScreen.main.bounds.width - 8)
+        .background {
+            RoundedRectangle(cornerRadius: 20)
+                .foregroundStyle(.viewBackground)
         }
     }
 }
