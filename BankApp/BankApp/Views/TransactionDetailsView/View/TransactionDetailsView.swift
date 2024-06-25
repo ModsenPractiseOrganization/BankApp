f77c85dfca2f5a0465d32e555569a9c3fd06bdba
Paul <pasha.makey@gmail.com>
2024-06-26 01:05:28 +0300

    Made Info Cells

--- BankApp/BankApp/Views/TransactionDetailsView/View/TransactionDetailsView.swift
+++ BankApp/BankApp/Views/TransactionDetailsView/View/TransactionDetailsView.swift
@@ -8,11 +8,69 @@
 import SwiftUI
 
 struct TransactionDetailsView: View {
+    let transaction: Transaction
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 16) {
+            HStack {
+                Text("Transaction")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(.leading, 6)
+                
+                Spacer()
+            }
+            .padding(.bottom, 32)
+            
+            TransactionInfoCell(
+                title: "Transaction was applied in",
+                value: transaction.company
+            )
+            TransactionInfoCell(
+                title: "Transaction number",
+                value: transaction.number
+            )
+            TransactionInfoCell(
+                title: "Date",
+                value: transaction.getFormattedDate
+            )
+            TransactionInfoCell(
+                title: "Transaction status",
+                value: transaction.status.rawValue
+            )
+            TransactionInfoCell(
+                title: "Amount",
+                value: transaction.getFormattedAmount
+            )
+                        
+            Spacer()
+        }
+    }
+    
+    @ViewBuilder
+    private func TransactionInfoCell(title: String, value: String) -> some View {
+        
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.title3)
+            
+            HStack {
+                Text(value)
+                    .padding()
+                
+                Spacer()
+            }
+            .frame(width: UIScreen.main.bounds.width - 8)
+            .background {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(lineWidth: 1)
+            }
+        }
     }
 }
 
 #Preview {
-    TransactionDetailsView()
+    TransactionDetailsView(
+        transaction: RecentTransactionsProvider.getRecentTransactions().first!
+    )
 }
