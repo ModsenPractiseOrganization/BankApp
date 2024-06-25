407b6c52b1a9d771bba55fe19488d2047de1c0a3
Paul <pasha.makey@gmail.com>
2024-06-25 21:39:18 +0300

    Added Quotes

--- BankApp/BankApp/DataProviders/RecentTransactionsProvider.swift
+++ BankApp/BankApp/DataProviders/RecentTransactionsProvider.swift
@@ -11,49 +11,49 @@ struct RecentTransactionsProvider {
     static func getRecentTransactions() -> [Transaction] {
         [
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .executed,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .declined,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .inProgress,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .executed,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .executed,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .executed,
                 amount: 10.09
             ),
             Transaction(
-                company: #"OOO "Company"#,
+                company: #"OOO "Company""#,
                 number: "f4345jfshjek3454",
                 date: Date(),
                 status: .executed,
