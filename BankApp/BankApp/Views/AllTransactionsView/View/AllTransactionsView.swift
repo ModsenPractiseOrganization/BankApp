c7ef7579795531e94930bd9a220dbd9d658cc750
Paul <pasha.makey@gmail.com>
2024-06-27 00:55:24 +0300

    Added AllTransactionsView

--- BankApp/BankApp/Views/AllTransactionsView/View/AllTransactionsView.swift
+++ BankApp/BankApp/Views/AllTransactionsView/View/AllTransactionsView.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct AllTransactionsView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var currentTransaction: Transaction?
+    
+    let transactions: [Transaction]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color.black
+                    .ignoresSafeArea()
+                
+                ScrollView {
+                    VStack {
+                        ForEach(transactions) { transaction in
+                            VStack {
+                                TransactionView(transaction: transaction)
+                                if transaction != transactions.last {
+                                    Divider()
+                                }
+                            }
+                            .onTapGesture {
+                                currentTransaction = transaction
+                            }
+                            
+                        }
+                    }
+                    .padding()
+                    .frame(width: UIScreen.main.bounds.width - 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 15)
+                            .foregroundStyle(.viewBackground)
+                    }
+                    .sheet(item: $currentTransaction) { transaction in
+                        TransactionDetailsView(transaction: transaction)
+                            .presentationDetents([.large])
+                    }
+                }
+                .padding(.top, 20)
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "ellipsis.circle")
+                            .foregroundStyle(.white)
+                            .fontWeight(.semibold)
+                    }
+                }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("^")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                            .rotationEffect(.degrees(-90))
+                            .padding(.leading, 4)
+                    }
+                }
+            }
+            .navigationBarBackButtonHidden()
+            .navigationTitle("All Transactions")
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
-}
-
-#Preview {
-    AllTransactionsView()
 }
