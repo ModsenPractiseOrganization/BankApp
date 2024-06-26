621775542d4395a3bafa6e8e65a5810ecf35d624
Paul <pasha.makey@gmail.com>
2024-06-26 19:06:51 +0300

    Work with Steets

--- BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
+++ BankApp/BankApp/Views/CurrentAccountView/View/CurrentAccountView.swift
@@ -10,79 +10,78 @@ import SwiftUI
 struct CurrentAccountView: View {
     
     @State private var currentAccount = AccountsProvider.getAccounts().first
-    @State private var shouldPresentSheet = false
+    @State private var currentTransaction: Transaction?
     
     private let recentTransactions = RecentTransactionsProvider.getRecentTransactions()
     
     var body: some View {
-        
-        VStack {
-            HStack {
-                Text("Account")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding(.leading, 6)
+        NavigationStack {
+            VStack {
+                HStack {
+                    Text("Account")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .padding(.leading, 6)
+                    
+                    Spacer()
+                }
+                .padding(.top, 12)
                 
-                Spacer()
-            }
-            .padding(.top, 12)
-            
-            // ACCOUNT
-            Button {
-                
-            } label: {
-                CurrentAccountViewCell(currentAccount: currentAccount!)
-            }
-            
-            // RECENT TRANSACTIONS
-            HStack {
-                Text("Recent Transactions")
-                    .font(.title)
-                    .fontWeight(.bold)
-                
-                Spacer()
-                
+                // ACCOUNT
                 Button {
                     
                 } label: {
-                    Text("VIEW ALL")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
+                    CurrentAccountViewCell(currentAccount: currentAccount!)
                 }
-            }
-            .padding([.leading, .trailing], 6)
-            .padding(.top, 20)
-            
-            // LIST
-            VStack {
-                ForEach(recentTransactions.prefix(4), id: \.self) { transaction in
+                
+                // RECENT TRANSACTIONS
+                HStack {
+                    Text("Recent Transactions")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
                     Button {
-                        shouldPresentSheet.toggle()
+                        
                     } label: {
+                        Text("VIEW ALL")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                    }
+                }
+                .padding([.leading, .trailing], 6)
+                .padding(.top, 20)
+                
+                // LIST
+                VStack {
+                    ForEach(recentTransactions.prefix(4), id: \.self) { transaction in
                         VStack {
                             TransactionView(transaction: transaction)
                             if transaction != recentTransactions.prefix(4).last {
                                 Divider()
                             }
                         }
-                    }
-                    .sheet(isPresented: $shouldPresentSheet) {
-                        TransactionDetailsView(transaction: transaction)
+                        .onTapGesture {
+                            currentTransaction = transaction
+                        }
+                        
                     }
                 }
+                .padding()
+                .frame(width: UIScreen.main.bounds.width - 8)
+                .background {
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(.viewBackground)
+                }
+                
+                Spacer()
             }
-            .padding()
-            .frame(width: UIScreen.main.bounds.width - 8)
-            .background {
-                RoundedRectangle(cornerRadius: 15)
-                    .foregroundStyle(.viewBackground)
+            .sheet(item: $currentTransaction) { transaction in
+                TransactionDetailsView(transaction: transaction)
+                    .presentationDetents([.large])
             }
-            
-            Spacer()
+            .background(.black)
         }
     }
-}
-
-#Preview {
-    CurrentAccountView()
 }
